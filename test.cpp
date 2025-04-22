f876abde19fc019a0a0aa93d1e28a614e8fdaad2
Kashaf ud duja <tahirkashaf7@gmail.com>
2025-04-22 15:20:19 +0500

    DSA Series

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // shortcut key to comment down is "ctrl+/""
+    cout << "Kashad ud"<<" duja"<< endl;
+    int age = 25;
+    cout << age << endl;
+    return 0;
+}
